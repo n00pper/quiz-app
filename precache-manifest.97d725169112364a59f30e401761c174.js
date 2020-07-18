2bdfe68410928e1837d0b01e66478577be1d35ff
noopper <vartefol@gmail.com>
2020-07-18 23:45:48 +0200

    Updates

---
+++ precache-manifest.97d725169112364a59f30e401761c174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "872673b88bd26a7c18e1a72529945c2e",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "b6c689f0da9c05e189b5",
+    "url": "/quiz-app/static/css/main.b129600d.chunk.css"
+  },
+  {
+    "revision": "f36fb8dd8eb0a82508f0",
+    "url": "/quiz-app/static/js/2.58f035de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-app/static/js/2.58f035de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c689f0da9c05e189b5",
+    "url": "/quiz-app/static/js/main.548a411f.chunk.js"
+  },
+  {
+    "revision": "e15a1632907c25963bb7",
+    "url": "/quiz-app/static/js/runtime-main.4f483d45.js"
+  }
+]);
